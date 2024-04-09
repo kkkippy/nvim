6add331324752492b62f2d2ae363d6ad653c0118
kkkippy <kipkipinho@gmail.com>
2024-04-09 10:36:07 -0300

    close current tab keymap addition

--- lua/kippy/keymaps.lua
+++ lua/kippy/keymaps.lua
@@ -37,6 +37,9 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts);
 keymap("n", "<Tab>l", ":bnext<CR>", opts);
 keymap("n", "<Tab>h", ":bprevious<CR>", opts);
 
+-- Close current tab
+keymap("n", "<C-x>", ":bd<CR>", opts);
+
 -- Move text up and down
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts);
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts);
